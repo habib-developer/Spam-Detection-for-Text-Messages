351af89bdf7de121330cf40ec94966910a057590
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-09-16 21:58:11 +0500

    route page to spamdetection

--- Spam Detection/Startup.cs
+++ Spam Detection/Startup.cs
@@ -57,7 +57,7 @@ namespace Spam_Detection
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=SpamDetection}/{action=Predict}/{id?}");
             });
         }
     }
